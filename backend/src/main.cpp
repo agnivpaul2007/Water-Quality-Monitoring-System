4a8a7eb602b443cc130f2e9a49a746e1c16f8817
agnivpaul2007 <livinggamer04@gmail.com>
2026-09-11 22:38:22 +0530

    Prototyped TDS feature

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -2,45 +2,39 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 2
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature tempSensor(&oneWire);
-
-
 struct SensorData
 {
   float temperature;
   float turbidity;
+  float TDS;
 };
 SensorData data;
 
 int temperature_sensorPin = 2;
 int turbidity_sensorPin = A0;
+int tds_sensorPin = A1;
+
+OneWire oneWire(temperature_sensorPin);
+DallasTemperature tempSensor(&oneWire);
 
 void setup()
 {
   Serial.begin(9600);
   tempSensor.begin();
   pinMode(turbidity_sensorPin, INPUT);
-  pinMode(temperature_sensorPin, INPUT);
-}
-
-float analogreadSensor(int pin)
-{
-  int sensorValue = 0;
-  float sumvoltage = 0;
-  for(int i = 0; i < 10; i++)
-  {
-    sensorValue = analogRead(pin);
-    sumvoltage += sensorValue * (5.0 / 1023.0);
-  }
-  return sumvoltage/10.0;
+  pinMode(tds_sensorPin, INPUT);
 }
 
 float turbidityConversion(float voltage)
 {
   float turbudity = map(voltage, 0, 640, 100, 0) - 25;
-  return turbudity; // Example conversion for turbidity sensor
+  return turbudity;
+}
+
+float tdsConversion(float voltage)
+{
+  float tds = map(voltage, 0, 1023, 0, 1000);
+  return tds;
 }
 
 
@@ -49,6 +43,7 @@ void loop()
   tempSensor.requestTemperatures();
   data.temperature = tempSensor.getTempCByIndex(0);
   data.turbidity = turbidityConversion(analogRead(turbidity_sensorPin));
+  data.TDS = tdsConversion(analogRead(tds_sensorPin));
   Serial.print(F("{\"pH\":"));
   Serial.print("0");
   Serial.print(F(",\"temperature\":"));
@@ -56,7 +51,7 @@ void loop()
   Serial.print(F(",\"turbidity\":"));
   Serial.print(data.turbidity, 1);
   Serial.print(F(",\"tds\":"));
-  Serial.print("0");
+  Serial.print(data.TDS, 2);
   Serial.println(F("}"));
   delay(1000);
 }
